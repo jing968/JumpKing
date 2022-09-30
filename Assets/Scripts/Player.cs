f2f66dd72bdf0466076949ee9b565c680bab4fe9
jing968 <jackydeng1968@gmail.com>
2022-09-30 22:50:54 +1000

    Implemented basic player controls. Currently jumping has issues with player inputs can release arrow keys right afte release space bar to interupt jumping directions

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Player status
+    public bool jumping;
+    public bool airBorn;
+    public bool bouncy;
+    float movements;
+    Rigidbody2D player;
+    public LayerMask ground;
+    [Header("Player configurations")]
+    public float moveSpeed;
+    public float jumpSpeed;
+    public float maxJumpValue;
+    public float jumpValue;
+    [Header("Physics Materials")]
+    public PhysicsMaterial2D airbornedPhysics;
+    public PhysicsMaterial2D groundedPhysics;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkAirBorn();
+        handleMovement();
+        handleJump();
+    }
+
+    void checkAirBorn() {
+        // Check if player is airBorned
+        airBorn = ! Physics2D.OverlapBox(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - 0.5f),
+            new Vector2(0.9f, 0.4f), 0f, ground);
+    }
+
+    void handleMovement(){
+        // Get desired movements from player inputs
+        movements = Input.GetAxisRaw("Horizontal");
+        // Approve moveent request only if NOT airBorn and not holding down jump button
+        if (jumpValue == 0.0f && !airBorn) {
+            player.velocity = new Vector2(moveSpeed * movements, player.velocity.y);
+        }
+    }
+
+    void handleJump(){
+        // Increment jumpValue when player hold down jump button
+        if(Input.GetKey("space") && (!airBorn) && (!jumping)) {
+            // Cap jump value at certain point
+            if (jumpValue < maxJumpValue)jumpValue += 0.1f;
+        }
+
+        // Disable movements when jump button is held down
+        if(Input.GetKeyDown("space") && (!airBorn) && (!jumping) ) {
+            player.velocity= new Vector2(0.0f, player.velocity.y);
+        }
+
+        // Apply bouncy phycis if player is jumping
+        if (airBorn) {
+            player.sharedMaterial = airbornedPhysics;
+            bouncy = true;
+        } else {
+            player.sharedMaterial = groundedPhysics;
+            bouncy = false;
+        }
+
+        if (jumpValue >= 20f && (!airBorn)) {
+            float newX = moveSpeed * movements;
+            float newY = jumpSpeed * jumpValue;
+            player.velocity = new Vector2(newX, newY);
+            Invoke("resetJump", 0.2f);
+        }
+
+        // 
+        if (Input.GetKeyUp("space") ){
+            if (!airBorn) {
+                player.velocity = new Vector2(moveSpeed * movements, jumpSpeed * jumpValue);
+                jumpValue = 0.0f;
+            }
+            jumping = false;
+        }
+    }
+
+    void resetJump(){
+        jumping = true;
+        jumpValue = 0.0f;
+    }
+}
