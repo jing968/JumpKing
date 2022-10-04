0cf3ce446810bc97e80ec5f71b54869630e0dab6
jing968 <jackydeng1968@gmail.com>
2022-10-04 22:16:40 +1100

    Added first 7 scenes of the game. Added cameraTracking script to dynamically moves cameras when player enter a new scene.

---
+++ Assets/Scripts/CameraTracker.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTracker : MonoBehaviour
+{
+    // Hardcoded values that reflects how the map is configured 
+    // DO NOT CHANGE THESE
+    private float low = -0.6f;
+    private float high = 9.4f;
+    
+
+    public GameObject camera;
+    public GameObject player;
+    public float level;
+    private float lowerBound, upperBound;
+    
+
+    // Default camera height: 4.4
+    // Increment depth: 
+
+
+    void Start()
+    {
+        float diff = level * 11f;
+        lowerBound = low + diff;
+        upperBound = high + diff;
+    }
+
+    // Update is called once per frame
+    void Update() 
+    {
+        manageCamera();
+    }
+
+    void manageCamera(){
+        float playerY = player.transform.position.y;
+        if (lowerBound <= playerY && playerY < upperBound) {
+            // Move camera accordingly
+            float newY = 4.4f + (level * 11);
+            camera.transform.position = new Vector3(camera.transform.position.x, newY, -10);
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -16,6 +16,8 @@ public class Player : MonoBehaviour
     public float jumpSpeed;
     public float maxJumpValue;
     public float jumpValue;
+    public float defaultJumpValue;
+    public float jumpValueIncrements;
     [Header("Physics Materials")]
     public PhysicsMaterial2D airbornedPhysics;
     public PhysicsMaterial2D groundedPhysics;
@@ -44,7 +46,7 @@ public class Player : MonoBehaviour
         // Get desired movements from player inputs
         movements = Input.GetAxisRaw("Horizontal");
         // Approve moveent request only if NOT airBorn and not holding down jump button
-        if (jumpValue == 0.0f && !airBorn) {
+        if (jumpValue == defaultJumpValue && !airBorn) {
             player.velocity = new Vector2(moveSpeed * movements, player.velocity.y);
         }
     }
@@ -53,7 +55,7 @@ public class Player : MonoBehaviour
         // Increment jumpValue when player hold down jump button
         if(Input.GetKey("space") && (!airBorn) && (!jumping)) {
             // Cap jump value at certain point
-            if (jumpValue < maxJumpValue)jumpValue += 0.1f;
+            if (jumpValue < maxJumpValue)jumpValue += jumpValueIncrements;
         }
 
         // Disable movements when jump button is held down
@@ -70,6 +72,7 @@ public class Player : MonoBehaviour
             bouncy = false;
         }
 
+        // Force a jump if jump value exceed 20
         if (jumpValue >= 20f && (!airBorn)) {
             float newX = moveSpeed * movements;
             float newY = jumpSpeed * jumpValue;
@@ -81,7 +84,7 @@ public class Player : MonoBehaviour
         if (Input.GetKeyUp("space") ){
             if (!airBorn) {
                 player.velocity = new Vector2(moveSpeed * movements, jumpSpeed * jumpValue);
-                jumpValue = 0.0f;
+                jumpValue = defaultJumpValue;
             }
             jumping = false;
         }
@@ -89,6 +92,6 @@ public class Player : MonoBehaviour
 
     void resetJump(){
         jumping = true;
-        jumpValue = 0.0f;
+        jumpValue = defaultJumpValue;
     }
 }
